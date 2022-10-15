23cc6abe0ea6a0eb01592b4410165df592d08057
Markemann1 <markust96@gmail.com>
2022-10-15 20:25:41 +0200

    Fikset variabelmatte ish usikker på math_v_res1+2

--- model1.py
+++ model1.py
@@ -4,12 +4,12 @@ from pyomo.opt import SolverFactory
 
 def task1_model():
     # Set data
-    T1 = list(range(1, 25))
-    T2 = list(range(25, 49))
-    S = list(range(0, 4))
+    T1 = list(range(1, 25))     # Hour 1-24
+    T2 = list(range(25, 49))    # Hour 25-48
+    S = list(range(0, 4))       # Scenario 0-4
 
     # Parameters data
-    MP = 50                 # Start-value of market price  # TODO: Gang i objektivfunksjonen
+    MP = 50                 # Start-value of market price
     WV_end = 13000          # EUR/Mm^3
     prob = 0.2              # 1/5 pr scenario
     Q_max = 0.36            # Mm^3
@@ -17,9 +17,9 @@ def task1_model():
     E_conv = 0.000000981    # MWh/Mm^3
     V_max = 10              # Mm^3
     IF_1 = 0.18             # Mm^3/h, Inflow Stage 1
-    IF_2 = 0.09             # Mm^3/h, Inflow Stage 2  # TODO: Gang i objektivfunksjon med scenario (s)
+    IF_2 = 0.09             # Mm^3/h, Inflow Stage 2
     V_01 = 5                # Mm^3, initial water level, Stage 1
-    V_02 = 0                # Mm^3initial water level, Stage 2  # TODO: løse med constraint
+    V_02 = 0                # Mm^3initial water level, Stage 2
 
     model = pyo.ConcreteModel('Task 1b')
 
@@ -36,10 +36,10 @@ def task1_model():
     model.P_max = pyo.Param(initialize=P_max)           # Max power production of hydropower unit
     model.E_conv = pyo.Param(initialize=E_conv)         # Conversion of power, p, produced pr. discarged water, q
     model.V_max = pyo.Param(initialize=V_max)           # Max water capacity in reservoir
-    model.IF_1 = pyo.Param(initialize=IF_1)             # Inflow in stage 1, deterministic
+    model.IF_1 = pyo.Param(model.T, initialize=IF_1)    # Inflow in stage 1, deterministic
     model.IF_2 = pyo.Param(model.S, initialize=IF_2)    # Inflow in stage 2, stochastic
     model.V_01 = pyo.Param(model.T, initialize=V_01)    # Initial water level t = 1
-    model.V_02 = pyo.Param(model.T, initialize=V_02)    # Initial water level t = 24
+    model.V_02 = pyo.Param(model.T, initialize=V_02)    # Initial water level t = 24  # TODO: mulig fjerne, eller constr?
 
     # Declaring decision variables
     model.q1 = pyo.Var(model.T1, bounds=(0, Q_max))
@@ -58,7 +58,21 @@ def task1_model():
         return obj
     model.OBJ = pyo.Objective(rule=objective(model), sense=pyo.maximize)
 
+    def math_production1(model):
+        return sum(model.p1[t] == model.q1[t] * model.E_conv for t in model.T1)
+    model.constr_productionDependency1 = pyo.Constraint(rule=math_production1)
 
+    def math_production2(model):
+        return sum(model.p2[t] == model.q2[t] * model.E_conv for t in model.T2)
+    model.constr_productionDependency2 = pyo.Constraint(rule=math_production2)
+
+    def math_v_res1(model):  # TODO: Hvordan legge inn V0 = 5 i første t
+        return sum(model.v_res1[t] == model.V01[1] + model.IF_1 - model.q1[t] for t in model.T1)
+    model.constr_math_v_res1 = pyo.Constraint(rule=math_v_res1)
+
+    def math_v_res2(model):  # TODO: Hvordan legge inn v_res[24] = ? i første t
+        return sum(model.v_res2[t] == model.v_res1[24] + (model.IF_2 * s) - model.q2[t][s] for t in model.T2 for s in model.S)
+    model.constr_math_v_res2 = pyo.Constraint(rule=math_v_res2)
 
     # Declaring constraints
     def productionLimit1(model):  # We cannot produce more than P_max
