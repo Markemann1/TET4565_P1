e4917f39e872f9ea9ea3f5a973e7cbbb3b37b15f
Markemann1 <markust96@gmail.com>
2022-10-15 20:52:58 +0200

    Oppg 1 er kjørbar fra main

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 import pyomo.environ as pyo
 from pyomo.opt import SolverFactory
+from model1 import task1_model
+
+def main():
+    task1_model()
 
 
-# Hydropower unit
-V_0 = 5  # mm3
-V_MAX = 10  # mm3
-Q_MAX = 100  # m^3/s
-M3S_TO_MM3 = 3.6/1000  # mm3/m^3
-E_conv = 0.981  # MWh/m^3
-WV_end = 13000  # EUR/mm3
-
-scen = 'scenarios'
-t = list(range(0, 48))
-# Inflow & prices
-Inflow_24t = 50  # m^3/s
-Inflow_48t = 25*scen  # m^3/s
-N_Scenarios = 5  # 5 scenarios for the last 24 hours
-P_scenario = 1/5  # [per unit], probability each scenario
-Price = 50 + t[0]  # linearly increasing cost based on timestep, from 0 to 48 hour
+if __name__ == '__main__':
+    main()
 
