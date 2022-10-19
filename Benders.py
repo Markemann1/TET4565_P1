f1f19485f2d3497d37ae95a5b5a6177002307aa7
Markemann1 <markust96@gmail.com>
2022-10-19 14:22:13 +0200

    Pseudo benders

--- Benders.py
+++ Benders.py
@@ -1,6 +1,7 @@
 import pyomo.environ as pyo
 from pyomo.opt import SolverFactory
 
+"""
 cuts_dict = {}  # dict i dict: cut 0: {'slope':(a), 'constant':(b)} for alle cuts
 
 model = pyo.ConcreteModel()
@@ -24,4 +25,39 @@ List_of_cuts.append(iteration)
 cuts_dict[iteration] = {}
 cuts_dict[iteration]['Slope'] = 30*iteration
 cuts_dict[iteration]['Constraint'] = 700-iteration
+"""
+# ^ Noe notater fra Q&A timen ^
+
+def masterProblem("noe input, og dict/list of cuts elns"):
+
+    print("dette skal være et selvstendig optimeringsprogram")
+
+    return "noe outputs som skal inn i subproblem"
+
+def subProblem("noe input som kommer fra masterProblem()"):
+
+    print("dette skal være et selvstendig optimeringsprogram")
+
+    return "noe output vi trenger i masterproblem ++ for å generere cuts"
+
+
+def generate_cuts("noe input fra subProblem() og en Xi fra masterProblem()"):
+
+
+    return "trenger kanskje ikke returne noe. Skal appende det nye cuttet i liste av cuts"
+
+
+def Benders_loop("tror ikke den trenger input"):
+    "noe initialisering av list of cuts elns"
+
+    for i in len(1,2,3,4,5,6,7,8,9):
+        "returns som kommer fra master a, b, c" = masterProblem("input")
+
+
+        Dual, OBJ = subProblem("problem init data", x_i)
+
+        generate_cuts(Xi, d, e, f)
+
+        print("noe greier så man kan se resultatene underveis")
+
 
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pyomo.environ as pyo
 from pyomo.opt import SolverFactory
 from model1 import task1_model
+from Benders import Benders_loop
 
 
 def main():
     task1_model()
+
+    # oppgave 2
+    # Benders_loop()
 
 
 if __name__ == '__main__':
