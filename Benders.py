cd630bea5d914f9264fe67df7a18b1174b738888
Daniel Holt <daniesho@stud.ntnu.no>
2022-10-20 17:31:13 +0200

    Masterproblem (ikke helt ferdig)

--- Benders.py
+++ Benders.py
@@ -35,15 +35,16 @@ T2 = list(range(25, 49))  # Hour 25-48
 S = list(range(0, 5))  # Scenario 0-4
 
 # Parameters data
+M3S_TO_MM3 = 3.6 / 1000  # Convesion factor to MM^3
 MP = 50  # Start-value of market price
 WV_end = 13000  # EUR/Mm^3
 prob = 0.2  # 1/5 pr scenario
-Q_max = 0.36  # Mm^3
-P_max = 100  # MW
-E_conv = 0.000000981  # MWh/Mm^3
+Q_max = 100 * M3S_TO_MM3  # Mm^3
+P_max = 100  # MW (over 1 hour)
+E_conv = 0.981 / M3S_TO_MM3  # MWh/Mm^3
 V_max = 10  # Mm^3
-IF_1 = 0.18  # Mm^3/h, Inflow Stage 1
-IF_2 = 0.09  # Mm^3/h, Inflow Stage 2
+IF_1 = 50 * M3S_TO_MM3  # Mm^3/h, Inflow Stage 1
+IF_2 = 25 * M3S_TO_MM3  # Mm^3/h, Inflow Stage 2
 V_01 = 5  # Mm^3, initial water level, Stage 1
 
 #Useful dictionaries
@@ -52,12 +53,76 @@ Cuts_data = {}
 Premilimanry_results = {}
 
 def masterProblem(Cuts_data):
+    # Set data
+    T1 = list(range(1, 25))  # Hour 1-24
+    T2 = list(range(25, 49))  # Hour 25-48
+    S = list(range(0, 5))  # Scenario 0-4
 
-    "Declare sets"
-    "Declare parameters"
-    "Declare variables (define alpha with constraints -1000000 til +1000000"
-    "Objective function"
-    "constraints for stage 1 (add cuts from Cuts_data)"
+    # Parameters data
+    M3S_TO_MM3 = 3.6 / 1000  # Convesion factor to MM^3
+    MP = 50  # Start-value of market price
+    Q_max = 100 * M3S_TO_MM3  # Mm^3
+    P_max = 100  # MW (over 1 hour)
+    E_conv = 0.981 / M3S_TO_MM3  # MWh/Mm^3
+    V_max = 10  # Mm^3
+    IF_1 = 50 * M3S_TO_MM3  # Mm^3/h, Inflow Stage 1
+    V_01 = 5  # Mm^3, initial water level, Stage 1
+
+    # Useful dictionaries
+
+    v_res1_data = {}
+    Cuts_data = {}
+    Premilimanry_results = {}
+
+    # -------- Initiate masterproblem -------------
+    mastermodel = pyo.ConcreteModel()
+
+    # -------- Declaring sets ---------------------
+    mastermodel.T1 = pyo.Set(initialize=T1)
+
+    # -------- Declaring parameters ---------------
+    mastermodel.MP = pyo.Param(initialize=MP)
+    mastermodel.Q_max = pyo.Param(initialize=Q_max)  # Max discharge of water to hydropower unit
+    mastermodel.P_max = pyo.Param(initialize=P_max)  # Max power production of hydropower unit
+    mastermodel.E_conv = pyo.Param(initialize=E_conv)  # Conversion of power, p, produced pr. discarged water, q
+    mastermodel.V_max = pyo.Param(initialize=V_max)  # Max water capacity in reservoir
+    mastermodel.IF_1 = pyo.Param(initialize=IF_1)
+    mastermodel.V_01 = pyo.Param(initialize=V_01)
+
+    # -------- Declaring decision variables -------
+    mastermodel.alpha = pyo.Var(mastermodel.T1, bounds=(-1000000, 1000000))
+    mastermodel.q1 = pyo.Var(mastermodel.T1, bounds=(0, Q_max))
+    mastermodel.p1 = pyo.Var(mastermodel.T1, bounds=(0, P_max))
+    mastermodel.v_res1 = pyo.Var(mastermodel.T1, bounds=(0, V_max), initialize=mastermodel.V_01)
+
+    # -------- Declaring Objective function --------
+    def objective(mastermodel):
+        o1 = sum(mastermodel.p1[t] * (mastermodel.MP + t) for t in mastermodel.T1)  # t=(1,24) production * market price
+
+        obj = o1 + mastermodel.alpha
+        return obj
+    mastermodel.OBJ = pyo.Objective(rule=objective(mastermodel), sense=pyo.maximize )
+
+    # -------- Declaring constraints ---------------
+    def math_production1(mastermodel, t):  # production = water discharge * power equivalent
+        return mastermodel.p1[t] == mastermodel.q1[t] * mastermodel.E_conv
+    mastermodel.constr_productionDependency1 = pyo.Constraint(mastermodel.T1, rule=math_production1)
+
+    def math_v_res1(mastermodel, t):  # water reservoir = init volume + inflow - discharge
+        if t == 1:
+            return mastermodel.v_res1[t] == mastermodel.V_01 + mastermodel.IF_1 - mastermodel.q1[
+                t]  # time index is what has happened up until t == [number]
+        else:
+            return mastermodel.v_res1[t] == mastermodel.v_res1[t - 1] + mastermodel.IF_1 - mastermodel.q1[t]
+    mastermodel.constr_math_v_res1 = pyo.Constraint(mastermodel.T1, rule=math_v_res1)
+
+
+
+    # TODO: Må fikse constriant som legger inn cuts
+    def alpha_cuts(mastermodel, ):
+        return(mastermodel.alpha <= "obj + dual*(v_res - v_rest24) ")
+
+    # TODO : Løse masterproblem og returnere rervoirnivå t=24
     "Solve problem and return reservoir level for t=24"
     return(v_res1(24))
 
