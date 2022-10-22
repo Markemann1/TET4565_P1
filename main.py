e51c9d24a104540caff367c54c1d73488e18c87c
Markemann1 <markust96@gmail.com>
2022-10-22 19:08:19 +0200

    Litt rydding, oppgave 3 kan kjøres

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import pyomo.environ as pyo
 from pyomo.opt import SolverFactory
 from model1 import task1_model
 from Benders import Benders_loop
-
+from StochasticDP import SDP_loop
 
 
 def main():
-    task1_model()
+    # print('Oppgave 1:')
+    # task1_model()
+    #
+    # print('Oppgave 2:')
+    # Benders_loop()
 
-    # oppgave 2
-   # Benders_loop()
+    print('Oppgave 3:')
+    SDP_loop()
 
 
 if __name__ == '__main__':
